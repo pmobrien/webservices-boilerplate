07fe3e0157c1d52de7e792c9a18d02bbf77e76d8
Patrick O'Brien <obrienp@outlook.com>
2017-08-29 12:50:59 -0500

    configurable port

--- src/main/java/com/pmobrien/rest/Application.java
+++ src/main/java/com/pmobrien/rest/Application.java
@@ -3,6 +3,8 @@ package com.pmobrien.rest;
 import com.pmobrien.rest.exceptions.UncaughtExceptionMapper;
 import com.pmobrien.rest.services.impl.HelloWorldService;
 import com.pmobrien.rest.services.impl.OmniUsersService;
+import com.pmobrien.rest.util.DefaultObjectMapper;
+import java.util.Optional;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -12,7 +14,7 @@ import org.glassfish.jersey.servlet.ServletContainer;
 public class Application {
 
   public static void main(String[] args) throws Exception {
-    Server server = new Server(8080);
+    Server server = new Server(port());
     
     new ServletContextHandler(server, "/api").addServlet(
         new ServletHolder(
@@ -34,5 +36,9 @@ public class Application {
     } finally {
       server.destroy();
     }
+  }
+  
+  private static int port() {
+    return Integer.parseInt(Optional.ofNullable(System.getProperty("port")).orElse("8080"));
   }
 }
