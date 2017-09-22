364be928ed491985cd398d484a0eeab4013e6296
Patrick O'Brien <obrienp@outlook.com>
2017-09-22 10:04:31 -0500

    using serializer that is now public

--- src/main/java/com/pmobrien/rest/DefaultObjectMapper.java
+++ src/main/java/com/pmobrien/rest/DefaultObjectMapper.java
@@ -1,22 +1,15 @@
 package com.pmobrien.rest;
 
 import com.cleo.graph.pojo.GraphEntity;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.cleo.graph.pojo.GraphEntity.GraphEntitySerializer;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.Collection;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
-import org.apache.commons.lang3.StringUtils;
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
@@ -28,77 +21,5 @@ public class DefaultObjectMapper implements ContextResolver<ObjectMapper> {
         .configure(SerializationFeature.INDENT_OUTPUT, true)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .registerModule(new SimpleModule().addSerializer(GraphEntity.class, new GraphEntitySerializer()));
-  }
-  
-  private static class GraphEntitySerializer extends StdSerializer<GraphEntity> {
-
-    public GraphEntitySerializer() {
-      this(null);
-    }
-
-    public GraphEntitySerializer(Class<GraphEntity> t) {
-      super(t);
-    }
-
-    /**
-     * By default, if entities contain references to each other, the writeValueAsString method from ObjectMapper will
-     * attempt to recursively serialize these, eventually throwing a StackOverflow exception. Rather than just ignoring
-     * references to other entities, this serialize method will just replace the reference with that entity's UUID.
-     * 
-     * @param entity
-     * @param generator
-     * @param provider
-     * 
-     * @throws IOException
-     * @throws JsonProcessingException 
-     */
-    @Override
-    public void serialize(GraphEntity entity, JsonGenerator generator, SerializerProvider provider)
-        throws IOException, JsonProcessingException {
-      generator.writeStartObject();
-      
-      for(Method method : entity.getClass().getMethods()) {
-        if(!"getClass".equals(method.getName()) && method.getName().contains("get")) {
-          try {
-            Object value = method.invoke(entity);
-            if(value != null) {
-              String name = StringUtils.uncapitalize(
-                  method.getName().substring(method.getName().indexOf("get") + "get".length())
-              );
-              
-              if(value instanceof GraphEntity) {
-                generator.writeObjectFieldStart(name);
-                generator.writeStringField("uuid", ((GraphEntity)value).getUuid().toString());
-                generator.writeEndObject();
-              } else if(value instanceof Collection) {
-                generator.writeArrayFieldStart(name);
-                
-                for(Object item : ((Collection)value)) {
-                  if(item instanceof GraphEntity) {
-                    generator.writeStartObject();
-                    generator.writeStringField("uuid", ((GraphEntity)item).getUuid().toString());
-                    generator.writeEndObject();
-                  } else {
-                    generator.writeObject(item);
-                  }
-                }
-                
-                generator.writeEndArray();
-              } else {
-                if(value instanceof Number) {
-                  generator.writeNumberField(name, ((Number)value).longValue());
-                } else {
-                  generator.writeStringField(name, value.toString());
-                }
-              }
-            }
-          } catch(ReflectiveOperationException ex) {
-            throw new RuntimeException(ex);
-          }
-        }
-      }
-      
-      generator.writeEndObject();
-    }
   }
 }
